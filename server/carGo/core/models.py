17f9672170b5ebc67a91c030078bfaa76319fcb8
macn1 <athulraihan27@gmail.com>
2025-06-04 18:18:41 +0530

    models changes

--- server/carGo/core/models.py
+++ server/carGo/core/models.py
@@ -13,12 +13,20 @@ class Expense(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10,decimal_places=2)
+    car = models.ForeignKey('Car', on_delete=models.SET_NULL, null=True, blank=True, related_name='expenses')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
     
 class Income(models.Model):
+    car = models.ForeignKey(
+        Car,
+        on_delete=models.SET_NULL,  # Don't delete income if car is deleted
+        null=True,               
+        blank=True,                
+        related_name='incomes'
+    )
     title = models.CharField(max_length=100)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10,decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
